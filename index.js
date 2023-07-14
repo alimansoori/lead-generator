44543ceb91ec70a1d89fcf2f9b088d597c26e26b
Ali <alimanssouri221@gmail.com>
2023-07-14 17:59:16 +0330

    Add webhook github

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let express = require( 'express' );
 let bb = require( 'express-busboy' );
 require('source-map-support').install();
 let db = require('./db');
+const simpleGit = require("simple-git");
 
 let app = express();
 app.set( 'trust proxy', true );
@@ -40,10 +41,58 @@ app.use( function( req, res ) {
 		.send('Sorry cant find that!');
 } );
 
+app.post('/webhook', async (req, res) => {
+	const payload = req.body;
+
+	// بررسی نوع رویداد بر اساس هدرهای دریافتی
+	const eventType = req.headers['x-github-event'];
+	if (eventType === 'push') {
+		try {
+			// انجام عملیات pull
+			const git = simpleGit();
+			await git.pull();
+
+			console.log('Pull operation completed');
+		} catch (error) {
+			console.error('Error occurred during pull operation:', error);
+		}
+	}
+
+	res.sendStatus(200);
+});
+
 // Listening
-app.listen( 8081, '0.0.0.0', function () {
+ const server = app.listen( 8081, '0.0.0.0', function () {
     console.log( 'DataTables Editor demo - navigate to http://localhost:8081/' );
 } );
+
+// راه‌اندازی مجدد وب سرویس با npm
+const restartWebServer = () => {
+	server.close(() => {
+		console.log('Web server stopped');
+
+		// اجرای دستور npm start برای راه‌اندازی مجدد وب سرویس
+		const { exec } = require('child_process');
+		exec('npm start', (error, stdout, stderr) => {
+			if (error) {
+				console.error('Failed to restart web server:', error);
+				return;
+			}
+
+			console.log('Web server restarted');
+		});
+	});
+};
+
+// راه‌اندازی مجدد وب سرویس در صورت دریافت رویداد push
+app.post('/webhook', (req, res) => {
+	const eventType = req.headers['x-github-event'];
+	if (eventType === 'push') {
+		restartWebServer();
+	}
+
+	res.sendStatus(200);
+});
 
 // Test the database connection on startup by getting a list of table names in the db
 // This can safely be removed if you are happy with your db connection configuration.
